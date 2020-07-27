b8f487b2c541e2f56236fd36a602615e5039585f
Michelle Hottinger <michhottinger@gmail.com>
2020-07-27 12:25:09 -0700

    added the solution

--- detect_cycle.py
+++ detect_cycle.py
@@ -1,2 +1,19 @@
-detect if linked list is in a cycle
+#detect if linked list is in a cycle
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        cur = head #start at head of list
+        dict = {}#create a cool dict for adding nodes
+        while(cur):
+            if(cur in dict):
+                return cur#return the value if its in the dict already
+            else:
+                dict[cur] = True#otherwise keep going forward
+            cur = cur.next
+        return None
