690e99ed52f5bac21c019f1d65e096ba4480fac7
Michelle Hottinger <michhottinger@gmail.com>
2020-07-27 12:23:45 -0700

    added the solution

--- duplicate.py
+++ duplicate.py
@@ -1,1 +1,5 @@
 #this can hold my duplicate problem from leetcode
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(set(nums)) != len(nums):
+            return True
